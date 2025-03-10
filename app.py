3296d27568139b828a19b7d5aebd53de27852338
Nakul Makode <134138075+NAKULMAK05@users.noreply.github.com>
2025-03-10 22:10:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ import time
 import pyttsx3
 import requests
 import streamlit as st
-import pandas as pd
-import numpy as np
 
 # Set up the Gemini API key and endpoint from google gemini Website
 api_key = "INSERT YOUR GEMINI API KEY HERE (OBTAIN IT FOR FREE FROM GOOGLE API DEVELOPER )"
